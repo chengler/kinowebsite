68e240e75a37f7ebd7d477d11319ff61c3aa7400
chengler <christian.engler@gmx.de>
2023-07-27 12:36:35 +0200

    update

--- hostsharing/firstCopy.bat
+++ hostsharing/firstCopy.bat
@@ -7,12 +7,12 @@ echo "Keine Garantie!"
 # exit 0
 
 cd .
-echo ##################################################
 echo kopiere default Logo und Favicon <= durch eigene ersetzen
-cp -v ..\static\lokal\favicon-default.ico ..\static\lokal\favicon.ico
-cp -v ..\static\lokal\logo-default.png  ..\static\lokal\KinoLogo.png
-echo ###################################################
+copy -v ..\static\lokal\favicon-default.ico ..\static\lokal\favicon.ico
+copy -v ..\static\lokal\logo-default.png  ..\static\lokal\KinoLogo.png
 echo kopiere newsletter_keys und privat_settings.py
-cp -v ..\mysite\configure_newsletter_keys.py ..\mysite\newsletter_keys.py
-cp -v ..\mysite\configure_privat_settings.py ..\mysite\privat_settings.py
-echo fertig
+copy -v ..\mysite\configure_newsletter_keys.py ..\mysite\newsletter_keys.py
+copy -v ..\mysite\configure_privat_settings.py ..\mysite\privat_settings.py
+cd ..\mysite
+echo ######
+echo 'privat_settings.py' und 'newsletter_keys' müssen angepasst werden!
