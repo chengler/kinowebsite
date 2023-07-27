3ee2ae6b353ae0a35107854a8f78ec2c68fecc2d
chengler <christian.engler@gmx.de>
2023-07-27 12:25:41 +0200

    update

--- hostsharing/firstCopy.sh
+++ hostsharing/firstCopy.sh
@@ -7,12 +7,10 @@ echo "Keine Garantie!"
 # exit 0
 
 cd .
-echo ##################################################
 echo kopiere default Logo und Favicon <= durch eigene ersetzen
-cp -v  ../static/lokal/favicon-default.ico  ../static/lokal/favicon.ico
-cp -v   ../static/lokal/logo-default.png  ../static/lokal/KinoLogo.png
-echo ###################################################
+copy -v  ../static/lokal/favicon-default.ico  ../static/lokal/favicon.ico
+copyp -v   ../static/lokal/logo-default.png  ../static/lokal/KinoLogo.png
 echo kopiere newsletter_keys und privat_settings.py
-cp -v ../mysite/configure_newsletter_keys ../mysite/newsletter_keys # Dort sind die Keys um Newsletters zu versenden
-cp -v ../mysite/configure_privat_settings.py ../mysite/privat_settings.py # Debug Mode, Log Level, Maintenance Mode, Credentials zum Mailserver usw. 
+copy -v ../mysite/configure_newsletter_keys ../mysite/newsletter_keys # Dort sind die Keys um Newsletters zu versenden
+copy -v ../mysite/configure_privat_settings.py ../mysite/privat_settings.py # Debug Mode, Log Level, Maintenance Mode, Credentials zum Mailserver usw. 
 echo fertig
