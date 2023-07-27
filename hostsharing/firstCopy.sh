30dc838cc31842e354a93c29def1862ba0bb0411
chengler <christian.engler@gmx.de>
2023-07-27 15:24:41 +0200

    cp -v ../mysite/configure_newsletter_keys.py ../mysite/newsletter_keys.py # Dort sind die Keys um Newsletters zu versenden .py ergänzt

--- hostsharing/firstCopy.sh
+++ hostsharing/firstCopy.sh
@@ -10,8 +10,8 @@ cd .
 echo kopiere default Logo und Favicon <= durch eigene ersetzen
 cp -v  ../static/lokal/favicon-default.ico  ../static/lokal/favicon.ico
 cp -v   ../static/lokal/logo-default.png  ../static/lokal/KinoLogo.png
-cp kopiere newsletter_keys und privat_settings.py
-cp -v ../mysite/configure_newsletter_keys ../mysite/newsletter_keys # Dort sind die Keys um Newsletters zu versenden
+echo cp kopiere newsletter_keys und privat_settings.py
+cp -v ../mysite/configure_newsletter_keys.py ../mysite/newsletter_keys.py # Dort sind die Keys um Newsletters zu versenden
 cp -v ../mysite/configure_privat_settings.py ../mysite/privat_settings.py # Debug Mode, Log Level, Maintenance Mode, Credentials zum Mailserver usw. 
 cd ../mysite/
 echo ######
